916bd1f09fe494b2d577903328975e1e613ae02c
Drew Howells <drew3141@gmail.com>
2024-03-20 22:22:54 -0600

    Added boxes to events in Agenda

--- app/src/main/java/com/example/androidapp_categoricalcalendar/views/AgendaView.kt
+++ app/src/main/java/com/example/androidapp_categoricalcalendar/views/AgendaView.kt
@@ -1,8 +1,11 @@
 package com.example.androidapp_categoricalcalendar.views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,8 +15,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.androidapp_categoricalcalendar.data.Event
 import com.example.androidapp_categoricalcalendar.data.EventDao
@@ -27,15 +33,27 @@ fun AgendaView(navController: NavHostController, eventDao: EventDao) {
     runBlocking {
         events = eventDao.getAll()
     }
+    Column {
+        Text(
+            text = "Agenda",
+            fontSize = 30.sp,
+            color = Color.White,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(10.dp)
+        )
+        LazyColumn {
+            items(events) {event ->
+                Box (
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .fillMaxWidth()
+                        .height((50 * (event.duration / 15)).dp)
+                        .background(color = Color.LightGray)
 
-    LazyColumn {
-        items(events) {event ->
-            Box (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height((50*(event.duration/15)).dp)
-            ){
-                Text(text = event.title)
+                ){
+                    Text(text = event.title)
+                }
             }
         }
     }
